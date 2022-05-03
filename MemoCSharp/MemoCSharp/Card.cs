9a42b88d83b07eb4d287f5c38c60c0f05f334dd5
pancho404 <franciscosanchez2101@gmail.com>
2022-05-03 10:25:27 -0300

    Created Card Class.

---
+++ MemoCSharp/MemoCSharp/Card.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MemoCSharp
+{
+    class Card
+    {
+        private char Symbol;
+        private bool IsDown;
+        private ConsoleColor Color;
+
+        public Card(char Symbol, ConsoleColor Color)
+        {
+            this.Symbol = Symbol;
+            Color = ConsoleColor.White;
+        }
+        public void Rotate(char NewSymbol, ConsoleColor NewColor)
+        {
+            Symbol = NewSymbol;
+            Color = NewColor;
+        }
+        
+    }
+}
