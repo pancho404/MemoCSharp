c5606685d3cfef49f5299a73a707c5a86f2d0b4f
pancho404 <franciscosanchez2101@gmail.com>
2022-05-03 10:09:54 -0300

    Initial Commit.

---
+++ MemoCSharp/MemoCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MemoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
